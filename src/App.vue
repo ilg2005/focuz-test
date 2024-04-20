c1f5b4335fa4c8af7e9e6760187bb7cdfb8d92f3
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-04-20 13:12:05 +0300

    styling

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,28 @@
     <draggable v-model="categoriesWithMeals" tag="ul" itemKey="category" group="categories" handle=".handle1"
                animation="300">
       <template #item="{ element: categoryWithMeals }">
-        <li class="bg-gray-100 p-2 rounded mb-4 shadow relative">
-          <div class="flex self-center">
-
-            <div @click="toggleVisibility(categoryWithMeals)">
-              <FoldButton v-if="categoryWithMeals.isVisible" class="mr-2"/>
-              <UnfoldButton v-else class="handle1 mr-2"/>
+        <div>
+          <div class="bg-gray-100 p-2 rounded mb-4 shadow relative">
+            <div class="flex self-center">
+              <div @click="toggleVisibility(categoryWithMeals)">
+                <FoldButton v-if="categoryWithMeals.isVisible" class="mr-2"/>
+                <UnfoldButton v-else class="mr-2"/>
+              </div>
+              <span>{{ categoryWithMeals.category }}</span>
             </div>
-            <span>{{ categoryWithMeals.category }}</span>
+            <MoveButton class="handle1 top-4 right-5"/>
           </div>
 
-          <draggable v-if="categoryWithMeals.isVisible" v-model="categoryWithMeals.meals" tag="ul" itemKey="meal" group="meals" class="mb-4 flex-grow"
+          <draggable v-if="categoryWithMeals.isVisible" v-model="categoryWithMeals.meals" tag="ul" itemKey="meal" group="meals" class="mb-4 mr-0 flex-grow w-[95%] mx-auto"
                      handle=".handle2" animation="300">
             <template #item="{ element: meal }">
-              <li class="bg-green-200 p-2 rounded mb-2 shadow relative">
+              <div class="bg-green-200 p-2 rounded mb-2 shadow relative">
                 {{ meal }}
                 <MoveButton class="handle2 top-1/2 transform -translate-y-1/2"/>
-              </li>
+              </div>
             </template>
           </draggable>
-          <MoveButton class="handle1 top-3 right-7"/>
-        </li>
+        </div>
       </template>
     </draggable>
   </div>
