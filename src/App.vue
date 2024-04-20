6703a7332076ba9c69ef0c45730aa1e6797dca70
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-04-20 13:51:21 +0300

    almost ready for Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="container mx-auto p-4">
-    <draggable v-model="categoriesWithMeals" tag="ul" itemKey="category" group="categories" handle=".handle1"
+    <draggable v-model="categoriesWithDocs" tag="ul" itemKey="category" group="categories" handle=".handle1"
                animation="300">
-      <template #item="{ element: categoryWithMeals }">
-        <div>
-          <div class="bg-gray-100 p-2 rounded mb-4 shadow relative">
+      <template #item="{ element: categoriesWithDocs }">
+        <li>
+          <div class="p-2 border relative">
             <div class="flex self-center">
-              <div @click="toggleVisibility(categoryWithMeals)">
-                <FoldButton v-if="categoryWithMeals.isVisible" class="mr-2"/>
+              <div @click="toggleVisibility(categoriesWithDocs)">
+                <FoldButton v-if="categoriesWithDocs.isVisible" class="mr-2"/>
                 <UnfoldButton v-else class="mr-2"/>
               </div>
-              <span>{{ categoryWithMeals.category }}</span>
+              <span class="font-bold">{{ categoriesWithDocs.category }}</span>
             </div>
             <MoveButton class="handle1 top-4 right-5"/>
           </div>
 
-          <draggable v-if="categoryWithMeals.isVisible" v-model="categoryWithMeals.meals" tag="ul" itemKey="meal" group="meals" class="mb-4 mr-0 flex-grow w-[95%] mx-auto"
+          <draggable v-if="categoriesWithDocs.isVisible" v-model="categoriesWithDocs.docs" tag="ul" itemKey="doc" group="docs" class="mr-0 flex-grow w-[95%] mx-auto"
                      handle=".handle2" animation="300">
-            <template #item="{ element: meal }">
-              <div class="bg-green-200 p-2 rounded mb-2 shadow relative">
-                {{ meal }}
+            <template #item="{ element: doc }">
+              <li class="p-2 border relative">
+                {{ doc }}
                 <MoveButton class="handle2 top-1/2 transform -translate-y-1/2"/>
-              </div>
+              </li>
             </template>
           </draggable>
-        </div>
+        </li>
       </template>
     </draggable>
   </div>
@@ -37,21 +37,27 @@ import MoveButton from "@/components/MoveButton.vue";
 import FoldButton from "@/components/FoldButton.vue";
 import UnfoldButton from "@/components/UnfoldButton.vue";
 
-const categoriesWithMeals = ref([
+const categoriesWithDocs = ref([
   {
-    category: 'Favorite Foods',
-    meals: ['Hamburger', 'Pizza', 'Spaghetti', 'Tacos', 'Teriyaki Chicken'],
+    category: 'Обязательные для всех',
+    docs: ['Паспорт', 'ИНН'],
     isVisible: true,
   },
   {
-    category: 'Terrible Foods',
-    meals: ['Bat wing soup', 'Spicy Octopus', 'Anything from Taco Bell'],
+    category: 'Обязательные для трудоустройства',
+    docs: ['Заявление о приеме на работу', 'Трудовой договор', 'Правила внутреннего распорядка'],
     isVisible: true,
-  }
+  },
+  {
+    category: 'Специальные',
+    docs: ['Специальный документ 1', 'Специальный документ 2'],
+    isVisible: true,
+  },
+
 ]);
 
 
-const toggleVisibility = (categoryWithMeals) => {
-  categoryWithMeals.isVisible = !categoryWithMeals.isVisible;
+const toggleVisibility = (categoriesWithDocs) => {
+  categoriesWithDocs.isVisible = !categoriesWithDocs.isVisible;
 };
 </script>
